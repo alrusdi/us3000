9f509b42d93815007770fddc26d7c2c87ad34dbd
Anatoly Plotnikov <anatolyplotnikov1@gmail.com>
2018-08-18 15:59:19 +0200

    exceptions handling changed in 'get_dict' function

--- words/management/commands/_get_dict_article.py
+++ words/management/commands/_get_dict_article.py
@@ -4,20 +4,28 @@ import os
 from ._words import words
 
 
-def get_dict(word):
+def get_dict(word_id):
     app_id = settings.OXFORD_DICTIONARY_APP_ID
     app_key = settings.OXFORD_DICTIONARY_APP_KEY
     language = 'en'
-    word_id = word
-
     url = '/'.join(('https://od-api.oxforddictionaries.com:443/api/v1/entries',
                     language, word_id.lower()))
+    response_text = ''
+
     try:
         r = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key})
-        status_code, text = r.status_code, r.text
+        r.raise_for_status()
+        status_message = 'Data successfully saved'
+        response_text = r.text
     except requests.exceptions.ConnectionError:
-        status_code, text = 434, ''
-    return status_code, text
+        status_message = 'Connection error'
+    except requests.exceptions.HTTPError as err:
+        if r.status_code == 404:
+            status_message = 'Specified word does not exist in Oxford Dictionary'
+        else:
+            status_message = 'HTTP error occurred. Response is: {}'.format(
+                err.response.content.decode())
+    return status_message, response_text
 
 
 def get_abs_file_path(filename):
@@ -32,17 +40,10 @@ def save_dict(file_path, word_dict):
 
 def save_article():
     word = words[0]
-    status_code, word_dict = get_dict(word)
-    if status_code == 200:
+    status_message, word_dict = get_dict(word)
+    if word_dict != '':
         abs_file_path = get_abs_file_path(word)
         save_dict(abs_file_path, word_dict)
-        status_message = 'Data successfully saved'
-    elif status_code == 404:
-        status_message = 'Word is not exist in Oxford Dictionary'
-    elif status_code == 434:
-        status_message = 'Oxford Dictionary is not available'
-    else:
-        status_message = 'Unknown error occurred'
     return status_message
 
 
