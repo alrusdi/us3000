c69a09577c05a6d00419a76d6d22b2128e89175d
Anatoly Plotnikov <anatolyplotnikov1@gmail.com>
2018-08-22 12:32:56 +0200

    got update of '_od_importer.py' and 'import_words.py' modules from master

--- words/management/commands/_od_importer.py
+++ words/management/commands/_od_importer.py
@@ -1,15 +1,11 @@
 import requests
-import os
-from django.conf import settings
 
 
 class Word:
     def __init__(self, word):
         self.word = word
 
-    def get_dict(self):
-        app_id = settings.OXFORD_DICTIONARY_APP_ID
-        app_key = settings.OXFORD_DICTIONARY_APP_KEY
+    def get_article(self, app_id, app_key):
         language = 'en'
         url = '/'.join(('https://od-api.oxforddictionaries.com:443/api/v1/entries',
                         language, self.word.lower()))
@@ -19,7 +15,8 @@ class Word:
             return "Word contains space", response_text
 
         try:
-            r = requests.get(url, headers={'app_id': app_id, 'app_key': app_key})
+            r = requests.get(url, headers={'app_id': app_id,
+                                           'app_key': app_key})
             r.raise_for_status()
             status_message = 'Data successfully saved'
             response_text = r.text
@@ -34,17 +31,16 @@ class Word:
                 raise
         return status_message, response_text
 
-    def get_abs_file_path(self):
-        full_filename = '.'.join((self.word, 'json'))
-        return os.path.join(settings.BASE_DIR, 'media', 'od', full_filename)
+    def make_abs_path(self, abs_dir_path):
+        return '{}/{}.json'.format(abs_dir_path, self.word)
 
-    def save_dict(self, file_path, word_dict):
+    def save_article(self, file_path, word_dict):
         with open(file_path, 'w') as f:
             f.write(word_dict)
 
-    def create_word_article(self):
-        status_message, word_dict = self.get_dict()
-        if word_dict != '':
-            abs_file_path = self.get_abs_file_path()
-            self.save_dict(abs_file_path, word_dict)
+    def create_word_article(self, abs_dir_path, app_id, app_key):
+        status_message, word_article = self.get_article(app_id, app_key)
+        if word_article != '':
+            abs_file_path = self.make_abs_path(abs_dir_path)
+            self.save_article(abs_file_path, word_article)
         return status_message
--- words/management/commands/import_words.py
+++ words/management/commands/import_words.py
@@ -1,5 +1,7 @@
 from django.core.management.base import BaseCommand
-from ._create_dict_article import Word
+from django.conf import settings
+import os
+from ._od_importer import Word
 from ._words import words
 
 
@@ -7,8 +9,14 @@ class Command(BaseCommand):
     help = 'Import dictionary articles from Oxford Dictionary'
 
     def handle(self, *args, **options):
-        for i in range(3000):
+        dir_path = os.path.join(settings.BASE_DIR, 'media', 'od')
+        app_id = settings.OXFORD_DICTIONARY_APP_ID
+        app_key = settings.OXFORD_DICTIONARY_APP_KEY
+        number_of_requested_articles = 60
+        for i in range(number_of_requested_articles):
             word = Word(words[i])
-            response_status = word.create_word_article()
+            response_status = word.create_word_article(dir_path,
+                                                       app_id,
+                                                       app_key)
             self.stdout.write(self.style.SUCCESS('{}: {}'.format(
                 word.word, response_status)))
