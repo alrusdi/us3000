b70e94fa0e91053a509ff1b7e47f7619b96a2f74
Anatoly Plotnikov <anatolyplotnikov1@gmail.com>
2018-08-29 14:55:53 +0200

    got updatees from master branch

--- words/tests/test_management.py
+++ words/tests/test_management.py
@@ -5,6 +5,7 @@ from words.management.commands._forvo_importer import ForvoImporter
 from words.management.commands._od_importer import ODImporter
 import fudge
 import requests
+import os
 
 
 class FakeRequestsResponse:
@@ -55,24 +56,21 @@ class ODImporterTest(TestCase):
             requests.exceptions.HTTPError(http_error)))
 
     @fudge.patch('words.management.commands._od_importer.requests.get')
-    @fudge.patch('words.management.commands._od_importer.os.path.exists')
-    @fudge.patch('words.management.commands._od_importer.os.access')
     @fudge.patch('words.management.commands._od_importer.ODImporter.make_abs_path')
     @fudge.patch('words.management.commands._od_importer.ODImporter.save_article')
-    def test_positive_case(self, fake_get, fake_path_exists,
-                           fake_dir_access, fake_abs_path, fake_save_article):
+    def test_positive_case(self, fake_get, fake_abs_path, fake_save_article):
         url = 'https://od-api.oxforddictionaries.com'
         expected_headers = {'app_id': 'another_test_app_id',
                             'app_key': 'another_test_app_key'}
+        dir_path = os.path.join(settings.BASE_DIR, 'media',
+                                'od')
         fake_get.expects_call().with_args(arg.contains(
             url), headers=expected_headers).returns(
             FakeRequestsResponse('{"article": "article"}'))
-        fake_path_exists.expects_call().returns(True)
-        fake_dir_access.expects_call().returns(True)
         fake_abs_path.expects_call().returns('')
         fake_save_article.expects_call().returns(None)
         test_word = ODImporter('fifth')
-        msg = test_word.create_word_article('', 'another_test_app_id',
+        msg = test_word.create_word_article(dir_path, 'another_test_app_id',
                                             'another_test_app_key')
         self.assertEqual(msg, 'Data successfully saved')
 
