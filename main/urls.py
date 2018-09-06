4336ed2fde9b08d03516654d6274bf7212d88b33
Anatoly Plotnikov <anatolyplotnikov1@gmail.com>
2018-09-06 11:04:10 +0200

    initial code for frontend added

--- main/urls.py
+++ main/urls.py
@@ -18,9 +18,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from words import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.IndexView.as_view(), name="home")
 ]
 
 if settings.DEBUG:
