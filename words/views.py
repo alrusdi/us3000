bfc2281d365a9d7fa4f94204ed7c01df2125103e
Anatoly Plotnikov <anatolyplotnikov1@gmail.com>
2018-09-16 09:44:20 +0200

    some queries to db in words-views added

--- words/views.py
+++ words/views.py
@@ -1,9 +1,13 @@
 import datetime
+import random
+
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 
+from main.settings import WORDS_NUMBER
 from main.views import JsonView
+from words.models import Word, WordLearningState
 
 
 @method_decorator(login_required, name='dispatch')
@@ -14,7 +18,9 @@ class IndexView(TemplateView):
 @method_decorator(login_required, name='dispatch')
 class LearningStateView(JsonView):
     def get_context_data(self):
-        return {'a': 1.3, 'b': datetime.datetime.now(), 'c': 'привет'}
+        random_new_words = self.get_random_new_words()
+        words_to_repeat = self.get_words_to_repeat(5)
+        return {'a': words_to_repeat, 'b': datetime.datetime.now(), 'c': 'привет'}
         # 1. Если у пользователя нет изученных слов - показываем ему settings.WORDS_NUMBER случайных
         # 2. Если у пользователя есть изученные слова, то показываем ему settings.WORDS_NUMBER слов, состоящих из
         # N случайных новых слов и M слов на повторение, где M > 0, если всего изученных слов больше или равно
@@ -31,32 +37,35 @@ class LearningStateView(JsonView):
     def get_random_new_words(self):
         user = self.request.user
         # получаем id всех слов:
-        # word_ids = Word.objects.filter(is_active=True).values_list('id', flat=True)
+        word_ids = Word.objects.filter(is_active=True).values_list('id', flat=True)
         # получаем список id изученных слов данного пользователя:
-        #
-        # known_words_ids = ...
+        known_words_ids = WordLearningState.objects.filter(
+            user=self.request.user,
+            is_user_know_meaning=True,
+            is_user_know_pronunciation=True
+        ).values_list('id', flat=True)
         #
         # из списка всех слов вычитаем список изученных
-        # new_ids = set(word_ids) - set(known_words_ids)
-        # волучаем случайные N из них
-        # needed_ids = randon.choise(new_ids)
+        new_ids = set(word_ids) - set(known_words_ids)
+        # получаем N случайных из них
+        needed_ids = random.sample(new_ids, WORDS_NUMBER)
         # для каждого из этих N создаем объект LearningState
         new_states = []
         for id in needed_ids:
-            new_states.append(LearningState(user=user, ...)) # именованые аргументы
+            new_states.append(WordLearningState(user=user, word_id=id, usage_count=1))  #  именованые аргументы
         # возвращаем список этих N объектов
         # https://docs.djangoproject.com/en/dev/ref/models/querysets/#bulk-create
-        LearningState.objects.bulk_create(new_states)
-        pass
+        WordLearningState.objects.bulk_create(new_states)
+        return str(new_states)
 
     def get_words_to_repeat(self, number_words_to_repeat):
-        words = LearningState.objects.filter(
+        words = WordLearningState.objects.filter(
             user=self.request.user,
             is_user_know_meaning=True,
             is_user_know_pronunciation=True
         ).order_by('usage_count')
         words = words[:number_words_to_repeat]
-        return list(words)
+        return str(words)
 
 
 
