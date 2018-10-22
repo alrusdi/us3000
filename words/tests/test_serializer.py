b74c5e6ba2e54475a85f9f55358256ed976cb6bc
Anatoly Plotnikov <anatolyplotnikov1@gmail.com>
2018-10-22 20:13:17 +0700

    tests updated

--- words/tests/test_serializer.py
+++ words/tests/test_serializer.py
@@ -34,13 +34,60 @@ class SerializerTest(TestCase):
         self.assertEqual(len(test_word_dict['meanings']), 1)
 
     def test_assigns_best_audio_from_preferred_pronunciation_field(self):
+        test_word = Word.objects.create(
+            value='test_word',
+            spelling='spelling',
+            raw_od_article='raw_od_article'
+        )
+        test_word.pronunciation_set.create(
+            audio='test_1_mp3'
+        )
+        best_pronunc = test_word.pronunciation_set.create(
+            audio='test_2_mp3'
+        )
+        test_word.meaning_set.create(
+            value='test_value'
+        )
+        user = User.objects.create_user(
+            username='test_username',
+            email='{}@debugmail.io'.format('test_username'),
+            password='123456'
+        )
+        ls = test_word.wordlearningstate_set.create(
+            user=user,
+            preferred_pronunciation=best_pronunc
+        )
+        test_word_dict = serialize_learning_state(ls)
+        self.assertEqual(test_word_dict['audio'][1]['best'], True)
         # создать learning state у которого есть preferred pronunc
         # вызвать функцию серриальизации
         # убедиться что нужному аудио назначено best = True
-        pass
 
     def test_assigns_first_audio_as_best_for_new_learning_state(self):
+        test_word = Word.objects.create(
+            value='test_word',
+            spelling='spelling',
+            raw_od_article='raw_od_article'
+        )
+        test_word.pronunciation_set.create(
+            audio='test_1_mp3'
+        )
+        test_word.pronunciation_set.create(
+            audio='test_2_mp3'
+        )
+        test_word.meaning_set.create(
+            value='test_value'
+        )
+        user = User.objects.create_user(
+            username='test_username',
+            email='{}@debugmail.io'.format('test_username'),
+            password='123456'
+        )
+        ls = test_word.wordlearningstate_set.create(
+            user=user
+        )
+        test_word_dict = serialize_learning_state(ls)
+        self.assertEqual(test_word_dict['audio'][0]['best'], True)
         # создать learning state у которого НЕТ preferred pronunc
         # вызвать функцию серриальизации
         # убедиться что ПЕРВОМУ аудио назначено best = True
-        pass
