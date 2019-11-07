05aa56991b3147612a1cfa87e1db7c867046befb
alrusdi <alrusdi@gmail.com>
2019-11-07 12:13:46 +0500

    Cleanup clientside tests

--- words/tests/test_client.py
+++ words/tests/test_client.py
@@ -1,12 +1,8 @@
-import os
-from datetime import date
-
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.test import override_settings
 
 from main.base_selenium_test_case import BaseSeleniumTestCase
-from main.settings import BASE_DIR
 
 
 class IndexPageObject:
@@ -37,23 +33,3 @@ class IndexTest(BaseSeleniumTestCase):
         link.click()
         meanings_container = self.find_element(self.page_object.meanings_container)
         assert meanings_container.visible
-
-    def test_show_main_page(self):
-        if not settings.TEST_CLIENTSIDE_CODE:
-            return
-        screenshot_name = '{}_{}_'.format(__name__, date.today())
-        screenshot_path = os.path.join(
-            os.path.join(BASE_DIR, 'words', 'tests',
-                         'screenshots', screenshot_name))
-        password = 'test12345678'
-        user = User.objects.create_user(
-            username='test_username',
-            email='{}@debugmail.io'.format('test_username'),
-            password=password
-        )
-        self.force_login(user)
-        self.browser.screenshot(screenshot_path)
-
-    def test_shows_error_message_if_server_down(self):
-        # Запатчить вьюшку, что бы она рэйзила исключение (get_context_data)
-        pass
